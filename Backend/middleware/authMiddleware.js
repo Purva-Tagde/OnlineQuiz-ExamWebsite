f2ceffaa9ef15018cbbf47d63ef4a81ee530a0f6
Purva-Tagde <162147055+Purva-Tagde@users.noreply.github.com>
2025-11-10 22:06:38 +0530

    Add files via upload

---
+++ Backend/middleware/authMiddleware.js
@@ -0,0 +1,18 @@
+import jwt from "jsonwebtoken";
+
+export const protect = (req, res, next) => {
+  const token = req.headers.authorization?.split(" ")[1];
+
+  if (!token) return res.status(401).json({ message: "No token, authorization denied" });
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    // FIXED LINE:
+    req.user = decoded.id;  // because generateToken stores { id: userId }
+
+    next();
+  } catch (error) {
+    return res.status(401).json({ message: "Invalid token" });
+  }
+};
